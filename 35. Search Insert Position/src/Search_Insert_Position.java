0f08dcfa3ca554e06375160d3efcf4001b480bd9
Rafael <rafael.agashirinov@gmail.com>
2023-08-29 23:04:53 +0400

    I have added new file

---
+++ 35. Search Insert Position/src/Search_Insert_Position.java
@@ -0,0 +1,53 @@
+import java.util.Collection;
+import java.util.Arrays;
+
+public class Search_Insert_Position {
+    public static void main(String[] args){
+        /*
+        Given a sorted array of distinct integers and a target value,
+        return the index if the target is found.
+        If not, return the index where it would be if it were inserted in order.
+
+        You must write an algorithm with O(log n) runtime complexity.
+
+        Example 1:
+        Input: nums = [1,3,5,6], target = 5
+        Output: 2
+
+        Example 2:
+        Input: nums = [1,3,5,6], target = 2
+        Output: 1
+
+        Example 3:
+        Input: nums = [1,3,5,6], target = 7
+        Output: 4
+         */
+
+        int[] nums = {1,3,5,6};
+
+        int target = 2;
+
+        System.out.println(searchInsert(nums,target));
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+
+        int low_point = 0;
+        int high_point = nums.length - 1;
+
+        while (low_point <= high_point) {
+            int middle_index =low_point + (high_point - low_point) / 2;
+
+            if (nums[middle_index] == target) {
+                return middle_index;
+            } else if (nums[middle_index] < target) {
+                low_point = middle_index + 1;
+            } else {
+                high_point = middle_index - 1;
+            }
+        }
+
+        // At this point, low_point is the insertion point
+        return low_point;
+    }
+}
