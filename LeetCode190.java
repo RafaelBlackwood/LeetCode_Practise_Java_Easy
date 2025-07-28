553e8acdd6647b6558d573a5274e439dcce1ec04
RafaelBlackwood <rafael.agashirinov@gmail.com>
2025-07-28 18:44:44 +0200

    Uploading new solved tasks

---
+++ LeetCode190.java
@@ -0,0 +1,41 @@
+public class LeetCode190 {
+    public static void main(String[] args) {
+        /*
+        Note:
+        Note that in some languages, such as Java,
+        there is no unsigned integer type. In this case,
+        both input and output will be given as a signed
+        integer type. They should not affect your implementation,
+        as the integer's internal binary representation is
+        the same, whether it is signed or unsigned.
+        In Java, the compiler represents the signed integers
+        using 2's complement notation. Therefore, in Example 2
+        below, the input represents the signed integer -3 and
+        the output represents the signed integer -1073741825.
+
+        Example 1:
+        Input: n = 43261596
+        Output: 964176192
+        Explanation:
+        Integer	Binary
+        43261596	00000010100101000001111010011100
+        964176192	00111001011110000010100101000000
+        Example 2:
+        Input: n = 2147483644
+        Output: 1073741822
+        Explanation:
+        Integer	Binary
+        2147483644	01111111111111111111111111111100
+        1073741822	00111111111111111111111111111110
+        Constraints:
+        0 <= n <= 231 - 2
+        n is even.
+         */
+    }
+
+    static class Solution {
+        public int reverseBits(int n) {
+
+        }
+    }
+}
