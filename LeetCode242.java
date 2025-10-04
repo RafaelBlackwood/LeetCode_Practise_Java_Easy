b573eb3e6cfbfe7ffd0ad5b89bfa24c50bc28c6a
RafaelBlackwood <rafael.agashirinov@gmail.com>
2025-10-04 18:54:18 +0200

    Added new solution

--- LeetCode242.java
+++ LeetCode242.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+
 public class LeetCode242 {
     public static void main(String[] args) {
         //Given two strings s and t, return true if t is an anagram of s, and false otherwise.
@@ -8,11 +10,24 @@ public class LeetCode242 {
         //Example 2:
         //Input: s = "rat", t = "car"
         //Output: false
+
+        String s ="anagram";
+        String t = "nagaram";
+
+        System.out.println(Solution.isAnagram(s,t));
     }
 
     static class Solution {
-        public boolean isAnagram(String s, String t) {
+        public static boolean isAnagram(String s, String t) {
+            if(s.length() != t.length()) return false;
 
+           int[] frequency = new int[26];
+
+           for(char c : s.toCharArray()) frequency[c - 'a']++;
+           for(char c : t.toCharArray()) frequency[c - 'a']--;
+
+           for(int i : frequency) if(i != 0) return false;
+           return true;
         }
     }
 }
